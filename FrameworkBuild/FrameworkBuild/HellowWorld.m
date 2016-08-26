faa61c8be05c50392f8108e74280740db91ede81
ClementGu <gujun0305@126.com>
2016-08-26 15:08:43 +0800

    hello world framework

---
+++ FrameworkBuild/FrameworkBuild/HellowWorld.m
@@ -0,0 +1,21 @@
+//
+//  HellowWorld.m
+//  FrameworkBuild
+//
+//  Created by Clement_Gu on 16/8/26.
+//  Copyright © 2016年 clement. All rights reserved.
+//
+
+#import "HellowWorld.h"
+
+@implementation HellowWorld
+/**
+ *  类方法实现
+ */
++(void)alert
+{
+    
+    NSLog(@"Hellow World!");
+}
+
+@end
